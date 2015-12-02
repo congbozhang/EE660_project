bd7418d4be61d22122ee5eb882433bd1f5087bea
congbozhang <congbozh@usc.edu>
2015-12-02 15:17:29 +0800

    pre_processing done
    catigorical binarization + split training / testing data + normalizarion

--- preprocessing.m
+++ preprocessing.m
@@ -4,12 +4,12 @@ close all;
 
 input=load('train_zcb_preprocessed.csv');
 
-label_col=34;
+y_col=34;
 cat_fea_col=[6,7,8,9,10,11,13,14,17,18,19,22,24,30,31,32];
 cat_fea_dim=[8,10,2,4,4,6,12,4,8,18,2,2,6,2,2,5];
 
-features=input;
-features(:,[cat_fea_col,label_col])=[];
+x=input;
+x(:,[cat_fea_col,y_col])=[];
 
 %categorical features binarization
 for i=1:size(cat_fea_col,2)
@@ -17,6 +17,26 @@ for i=1:size(cat_fea_col,2)
    for j=1:size(input,1)
        temp(j,input(j,cat_fea_col(i)))=temp(j,input(j,cat_fea_col(i)))+1;  
    end
-   features=[features,temp];
+   x=[x,temp];
 end
-label_train=input(:,label_col);
+y=input(:,y_col);
+
+%splitting data into training set and testing set
+num_train=int32(size(x,1)/2);
+num_test=size(x,1)-num_train;
+indices=randperm(num_train);
+xtrain=x(indices,:);
+xtest=x;
+xtest(indices,:)=[];
+ytrain=y(indices,:);
+ytest=y;
+ytest(indices,:)=[];
+
+%normalization
+mean_train=mean(xtrain);
+std_train=std(xtrain);
+std_train(std_train==0)=0.001;
+xtrain=(xtrain-repmat(mean_train,size(xtrain,1),1))./repmat(std_train,size(xtrain,1),1);
+xtest=(xtest-repmat(mean_train,size(xtest,1),1))./repmat(std_train,size(xtest,1),1);
+
+save('C:\Users\Congbo\Desktop\EE660_project\data.mat','xtrain','xtest','ytrain','ytest');
